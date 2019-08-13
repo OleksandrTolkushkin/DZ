8d6cc7bd5ef587ef377de0fed0f2c687f0d73133
Alex19 <alex.tolkushkin@gmail.com>
2019-08-13 12:43:03 +0300

    Task 1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,9 @@
 package com.company;
-import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        int[] sourceNumbers = new int[size];
-        System.out.print("Введите целые числа через пробел ");
-        for (int i = 0; i < size; i++) {
-            sourceNumbers[i] = scanner.nextInt();
-        }
+        String name = scanner.next();
+        System.out.println("Privet" + name);
     }
-}
+    }
