c4d020f6eb30fa51906507b04fdf93b3c729810a
Alex19 <alex.tolkushkin@gmail.com>
2019-08-13 12:59:19 +0300

    CommitDZ2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,21 @@ package com.company;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+      /*  Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         System.out.println("Privet" + name);
         }
+    }*/
+        /*2. Отобразить в окне консоли аргументы командной строки в обратном порядке.*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите цифры: ");
+        int value = scanner.nextInt();
+        int mirror = 0;
+        while (value != 0) {
+            mirror = mirror * 10 + (value % 10);
+            value = value / 10;
+        }
+        System.out.print("Обратная последовательность цифр: " + mirror);
     }
+}
 
